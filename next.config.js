c43a8f5f5fc8821a0f22668837df01d6eb3a9b15
paperdog-org <131909245+paperdog-org@users.noreply.github.com>
2024-06-25 13:55:44 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
- /** @type {import('next').NextConfig} */
+/**  @type {import('next').NextConfig} */
+
  const nextConfig = {
-    output: "exports", // lightweight but no dynamic routes & api support
+    output: "export", // lightweight but no dynamic routes & api support
     // output: "standalone", // bulkier but supports dynamic routes & api
-}
+};
 
 module.exports = nextConfig
